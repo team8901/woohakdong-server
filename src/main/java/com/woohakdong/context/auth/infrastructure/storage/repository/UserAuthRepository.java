b5e62e9481a34820ff87c9fd6533d64301d4b38f
sangjun <sangjun1389@ajou.ac.kr>
2025-07-05 17:12:11 +0900

    Feat: 소셜 로그인 정보 기반 유저 등록

---
+++ src/main/java/com/woohakdong/context/auth/infrastructure/storage/repository/UserAuthRepository.java
@@ -0,0 +1,10 @@
+package com.woohakdong.context.auth.infrastructure.storage.repository;
+
+import com.woohakdong.context.auth.model.UserAuthEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAuthRepository extends JpaRepository<UserAuthEntity, Long> {
+
+    Optional<UserAuthEntity> findByAuthProviderAndAuthProviderUserId(String authProvider, String authProviderId);
+}
