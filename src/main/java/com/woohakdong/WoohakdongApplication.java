326553cc42fdc5dce1a673af3e27225f32967d58
sangjun <sangjun1389@ajou.ac.kr>
2025-07-05 15:09:07 +0900

    Init: 프로젝트 시작

---
+++ src/main/java/com/woohakdong/WoohakdongApplication.java
@@ -0,0 +1,13 @@
+package com.woohakdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoohakdongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoohakdongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woohakdong/WoohakdongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woohakdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoohakdongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
