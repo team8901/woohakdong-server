e290f291a7be09fc202416e82873df8135630632
sangjun <sangjun1389@ajou.ac.kr>
2025-07-05 18:53:41 +0900

    refactor: 구글 로그인

--- src/main/java/com/woohakdong/context/auth/domain/GoogleLoginProvider.java
+++ src/main/java/com/woohakdong/context/auth/domain/GoogleLoginProvider.java
@@ -1,31 +1,20 @@
 package com.woohakdong.context.auth.domain;
 
+import com.woohakdong.context.auth.infrastructure.client.GoogleOAuthClient;
 import com.woohakdong.context.auth.model.SocialUserInfo;
 import java.util.Map;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 @Component
+@RequiredArgsConstructor
 public class GoogleLoginProvider implements SocialLoginProvider {
-    private static final String GOOGLE_OAUTH_ENDPOINT = "https://www.googleapis.com/oauth2/v3/userinfo";
     private static final String GOOGLE = "google";
     private static final String FIELD_EMAIL = "email";
     private static final String FIELD_NAME = "name";
     private static final String FIELD_SUB = "sub";
 
-    private final RestTemplate restTemplate;
-
-    public GoogleLoginProvider(RestTemplateBuilder builder) {
-        this.restTemplate = builder.build();
-    }
+    private final GoogleOAuthClient oAuthClient;
 
     @Override
     public boolean supports(String provider) {
@@ -34,33 +23,10 @@ public class GoogleLoginProvider implements SocialLoginProvider {
 
     @Override
     public SocialUserInfo fetch(String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
-        ResponseEntity<Map<String, Object>> response = null;
-        try {
-            response = restTemplate.exchange(
-                    GOOGLE_OAUTH_ENDPOINT,
-                    HttpMethod.GET,
-                    entity,
-                    new ParameterizedTypeReference<>() {
-                    }
-            );
-        } catch (HttpClientErrorException e) {
-            if (e.getStatusCode().value() == HttpStatus.UNAUTHORIZED.value()) {
-                throw new IllegalStateException("Google OAuth 요청 실패: 인증되지 않은 토큰입니다.");
-            }
-            throw new IllegalStateException("Google OAuth 요청 실패: " + e.getResponseBodyAsString());
-        }
-
-        Map<String, Object> body = response.getBody();
+        Map<String, Object> body = oAuthClient.fetchGoogleOAuthPayload(accessToken);
         String name = extractField(body, FIELD_NAME);
         String email = extractField(body, FIELD_EMAIL);
         String providerUserId = extractField(body, FIELD_SUB);
-
-        if (email == null || email.isBlank()) {
-            throw new IllegalStateException("Google access token is invalid: email not found");
-        }
 
         return new SocialUserInfo(name, email, providerUserId, GOOGLE);
     }
