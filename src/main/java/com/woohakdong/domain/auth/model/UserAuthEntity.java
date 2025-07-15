9532e94d7ea657edfdc653d40ae97ddf130b25e8
sangjun <sangjun1389@ajou.ac.kr>
2025-07-15 22:39:40 +0900

    Fix: user_auth 테이블명 수정

--- src/main/java/com/woohakdong/domain/auth/model/UserAuthEntity.java
+++ src/main/java/com/woohakdong/domain/auth/model/UserAuthEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
+@Table(name = "user_auth")
 public class UserAuthEntity {
 
     @Id
