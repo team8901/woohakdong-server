00cc7e98f52f726a8cb5173a68ad88d8c8da23e9
sangjun <sangjun1389@ajou.ac.kr>
2025-07-21 23:49:55 +0900

    Chore: ListWrapper 필드명 변경

--- src/main/java/com/woohakdong/controller/dto/response/ListWrapper.java
+++ src/main/java/com/woohakdong/controller/dto/response/ListWrapper.java
@@ -3,7 +3,7 @@ package com.woohakdong.controller.dto.response;
 import java.util.List;
 
 public record ListWrapper<T>(
-        List<T> result
+        List<T> data
 ) {
     public static <T> ListWrapper<T> of(List<T> data) {
         return new ListWrapper<>(data);
